93f825adc5fa71ee12b9e9af030171e15c924a9e
Marco Gazzola <marcogazzola@MacBook-Pro-di-Marco-2.local>
2018-06-26 18:42:45 +0200

    .

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 var UserController = require('./user/UserController');
+var DemoController = require('./demo/DemoController');
+var IliadController = require('./Iliad/IliadController');
+
 app.use('/users', UserController);
+app.use('/demo', DemoController);
+app.use('/iliad', IliadController);
+
 module.exports = app;
