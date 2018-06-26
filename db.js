5f60252d30441f679b088717872d6b08995a474a
Marco Gazzola <marcogazzola@MacBook-Pro-di-Marco-2.local>
2018-06-26 11:28:03 +0200

    add config

--- db.js
+++ db.js
@@ -1,2 +1,4 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://marco:marco123@ds219051.mlab.com:19051/noderestapi');
+var config = require('./config');
+
+mongoose.connect(config.mongodb.url);
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var app = require('./app');
-var port = process.env.PORT || 3000;
+var config = require('./config');
+
+var port = config.web.port;
 var server = app.listen(port, function() {
   console.log('Express server listening on port ' + port);
 });
